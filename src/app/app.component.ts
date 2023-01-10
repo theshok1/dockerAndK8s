6d0b31020e52855ecd4fa04b41e8f1bbdc69fc9a
Александр Промский <alex.promskiy97@gmail.com>
2023-01-10 12:52:55 +0300

    ToDo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ViewChild, ElementRef } from '@angular/core';
+import { MatTableDataSource, MatTable } from '@angular/material/table'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dockerAndK8s';
+
+  @ViewChild('formTitle') formTitle!: ElementRef<any>
+  @ViewChild('comment') comment!: ElementRef<any>
+  @ViewChild('table') table!: MatTable<any>
+
+  toDoList: MatTableDataSource<{title: string, comment: string}> = new MatTableDataSource()
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+  addData(): void {
+    this.toDoList.data.push({title: this.formTitle.nativeElement.value, comment: this.comment.nativeElement.value})
+    this.table.renderRows()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCommonModule } from '@angular/material/core'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatTableModule } from '@angular/material/table'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCommonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
